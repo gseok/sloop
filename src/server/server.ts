83bedca18a92168bdae91f66cee325126c920ec2
GyeongSeokSeo <gyseok.seo@navercorp.com>
2020-02-05 10:29:20 +0900

    Add koa server ping api

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,21 @@
+/* eslint-disable no-console */
 import Koa from 'koa';
 import commonErrorHandler from './middlewares/commonErrorHandler';
 import pageNotFound from './middlewares/pageNotFound';
 import router from './routes';
 
+const PORT = 3131;
 const app: Koa = new Koa();
 
+// server main error handler
 app.use(commonErrorHandler);
-app.use(pageNotFound);
 
 // route
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000);
+// not supported route then show up page not found
+app.use(pageNotFound);
+
+app.listen(PORT);
+console.log(`Server is running at http://127.0.0.1:${PORT}`);
+console.log('Press CTRL-C to stop');
