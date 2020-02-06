80941c3347849de58081ffda51a381d0616e2b64
GyeongSeokSeo <gyseok.seo@navercorp.com>
2020-02-06 15:24:54 +0900

    Add styled component typescript build setting

--- scripts/build-scripts/webpack.config.client-only.js
+++ scripts/build-scripts/webpack.config.client-only.js
@@ -39,6 +39,9 @@ module.exports = (env) => {
           use: [
             {
               loader: 'babel-loader',
+              options: {
+                customize: require.resolve('babel-preset-react-app'),
+              },
             },
             {
               loader: 'ts-loader',
@@ -49,11 +52,12 @@ module.exports = (env) => {
           ],
         },
         {
-          test: /\.scss$/,
+          test: /\.module\.(scss|sass)$/,
           use: [
-            // { loader: 'style-loader' }, // to inject the result into the DOM as a style block
+            { loader: 'style-loader' }, // to inject the result into the DOM as a style block
             // { loader: 'css-modules-typescript-loader' }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
-            // { loader: 'css-loader', options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+            { loader: 'css-loader', options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+            { loader: 'resolve-url-loader' },
             { loader: 'sass-loader' }, // to convert SASS to CSS
           ],
         },
