92645c4e7b8440de27d47f4602246bbd11d1a003
GyeongSeokSeo <gyseok.seo@navercorp.com>
2020-02-08 07:06:19 +0900

    Add style module support

--- src/client/components/Loading.tsx
+++ src/client/components/Loading.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-// import css from './Loading.module.scss';
+import css from './Loading.module.scss';
 
-// const Loading = () => <div className={css.Loading}>Loading...</div>;
-const Loading = () => <div>Loading...</div>;
+const Loading = () => <div className={css.Loading}>Loading...</div>;
 
 export default Loading;
